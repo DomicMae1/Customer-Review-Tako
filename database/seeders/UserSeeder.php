8a437b5a4bd689812ce8b94a4d97d59863f9d2d0
DomicMae1 <ardon.yunors@tako.co.id>
2026-05-08 14:46:21 +0700

    fix: update seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,11 +18,11 @@ class UserSeeder extends Seeder
         }
 
         $users = [
-            ['name' => 'John Doe', 'email' => 'marketing@gmail.com', 'password' => '1234', 'role' => 'user',     'id_perusahaan' => 1],
-            ['name' => 'Rose Doe', 'email' => 'manager@gmail.com', 'password' => '1234', 'role' => 'manager',  'id_perusahaan' => 1],
-            ['name' => 'Emi Rina', 'email' => 'direktur@gmail.com', 'password' => '1234', 'role' => 'direktur', 'id_perusahaan' => 1],
-            ['name' => 'Tatsuya', 'email' => 'lawyer@gmail.com', 'password' => '1234', 'role' => 'lawyer',   'id_perusahaan' => 1],
-            ['name' => 'don8', 'email' => 'don8@gmail.com', 'password' => '1234', 'role' => 'admin'],
+            ['name' => 'John Doe', 'email' => 'marketing@gmail.com', 'password' => 'CR_tako@2025', 'role' => 'user',     'id_perusahaan' => 1],
+            ['name' => 'Rose Doe', 'email' => 'manager@gmail.com', 'password' => 'CR_tako@2025', 'role' => 'manager',  'id_perusahaan' => 1],
+            ['name' => 'Emi Rina', 'email' => 'direktur@gmail.com', 'password' => 'CR_tako@2025', 'role' => 'direktur', 'id_perusahaan' => 1],
+            ['name' => 'Tatsuya', 'email' => 'lawyer@gmail.com', 'password' => 'CR_tako@2025', 'role' => 'lawyer',   'id_perusahaan' => 1],
+            ['name' => 'Admin', 'email' => 'admin@gmail.com', 'password' => 'CR_tako@2025', 'role' => 'admin'],
             ['name' => 'David Marketing', 'email' => 'david.marketing@tako.co.id', 'password' => 'CR_tako@2025', 'role' => 'user'],
             ['name' => 'David Manager', 'email' => 'david.manager@tako.co.id', 'password' => 'CR_tako@2025', 'role' => 'manager'],
             ['name' => 'David Direktur', 'email' => 'david.direktur@tako.co.id', 'password' => 'CR_tako@2025', 'role' => 'direktur'],
